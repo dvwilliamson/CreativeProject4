24b0a4eede81452f372c7fa8a30bd1fc17d3034c
Dallas Williamson <dallasvw@Dallass-MacBook-Pro.local>
2018-03-02 07:17:38 -0700

    updating mask selection

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var app = new Vue({
-  el: '#app',
+var appHeader = new Vue({
+  el: '#appHeader',
   data: {
     navItems: {
       1: {
         href: '#intro',
-        title: 'Intro'
+        title: 'Welcome'
       },
       2: {
         href: '#one',
@@ -12,51 +12,73 @@ var app = new Vue({
       },
     },
   },
-  created: function() {
-    //this.xkcd();
-  },
-  computed: {
-    month: function() {
-      var month = new Array;
-      if (this.current.month === undefined)
-        return '';
-      month[0] = "January";
-      month[1] = "February";
-      month[2] = "March";
-      month[3] = "April";
-      month[4] = "May";
-      month[5] = "June";
-      month[6] = "July";
-      month[7] = "August";
-      month[8] = "September";
-      month[9] = "October";
-      month[10] = "November";
-      month[11] = "December";
-      return month[this.current.month - 1];
-    }
-  },
-  watch: {
-    number: function(value,oldvalue) {
-      //if (oldvalue === '') this.max = value;
-      //else this.xkcd();
-    },
-  },
-  methods: {
-    apiCall: function() {
-      this.loading = true;
-      //var url="https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=luchador";
-      fetch(url).then(response => {
-        return response.json();
-      }).then(json => {
-        console.log(json);
-        return true;
-      }).catch(err => {
-        console.log("Nooo!.1.1>");
-      });
-    },
-  }
 });
 
+var appOne = new Vue({
+  el: '#one',
+  data: {
+    curIndex: 0,
+    images: {
+      0:{
+        url: "/CreativeProject4/images/masks/la_muerte.svg",
+        description: "",
+        active: "",
+      },
+      1:{
+        url: "/CreativeProject4/images/masks/armadillo.svg",
+        description: "",
+        active: "",
+      },
+      2:{
+        url: "/CreativeProject4/images/masks/estrella_grande.svg",
+        description: "",
+        active: "",
+      },
+      3:{
+        url: "/CreativeProject4/images/masks/la_fila.svg",
+        description: "",
+        active: "",
+      },
+      4:{
+        url: "/CreativeProject4/images/masks/blue_demon.svg",
+        description: "",
+        active: "",
+      },
+    },
+    luchMask: "/CreativeProject4/images/masks/la_muerte.svg",
+  },
+  created: function(){
+  },
+  watch:{
+    curIndex: function(newVal, oldVal) {
+      maxLen = Object.keys(this.images).length
+      if (this.curIndex >= maxLen)
+        this.curIndex = 0;
+      else if (this.curIndex < 0)
+        this.curIndex = maxLen - 1;
+      this.luchMask = this.images[this.curIndex].url;
+
+      this.images[oldVal].active = false;
+      this.images[this.curIndex].active = true;
+
+    }
+  },
+  methods:{
+    next: function() {
+      this.curIndex += 1;
+    },
+    prev: function(){
+      this.curIndex -= 1;
+    },
+    goToMask: function(e, img){
+      this.curIndex = parseInt(this.objectKeyByValue(this.images, img));
+    },
+    objectKeyByValue: function(obj, val) {
+      return Object.entries(obj).find(i => i[1] === val);
+    }
+
+  },
+});
 
 
 
