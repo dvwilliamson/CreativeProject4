6b3608fb405461183aae89593bef8b6509f96acd
Dallas Williamson <dallasvw@Dallass-MacBook-Pro.local>
2018-03-02 09:45:50 -0700

    Added Mask Descriptions

--- script.js
+++ script.js
@@ -21,33 +21,49 @@ var appOne = new Vue({
     images: {
       0:{
         url: "/CreativeProject4/images/masks/la_muerte.svg",
-        description: "",
+        title:"La muerte",
+        description: "This mask is the manliest of the manliest that can only be \
+        ventured by those who are worthy of its power. The golden skull \
+        symbolizes Xolotl, the ancient aztec god of death. Fear by to any \
+        man who dare challenge the man wearing this mask.",
         active: "",
       },
       1:{
         url: "/CreativeProject4/images/masks/armadillo.svg",
-        description: "",
+        title: "El armadillo",
+        description: "Small, round, and fast, the bearer of this mask is \
+        a force to be reckoned with.",
         active: "",
       },
       2:{
         url: "/CreativeProject4/images/masks/estrella_grande.svg",
-        description: "",
+        title:"La estrella grande",
+        description: "This mask is for the ambitious underdog that once was \
+        told to shoot for the moon and even if he missed he would land among \
+        the start. Sadly, he did miss the moon and now he's willing to do \
+        anything to regain his honor. His thirst for winning is unquenchable.",
         active: "",
       },
       3:{
         url: "/CreativeProject4/images/masks/la_fila.svg",
-        description: "",
+        title: "La batería",
+        description: "Whoever wears this mask has a electric boost that \
+        allows him to gain the upper-hand against his foes.",
         active: "",
       },
       4:{
         url: "/CreativeProject4/images/masks/blue_demon.svg",
-        description: "",
+        title: "Demonio Azul",
+        description: "This is the mask that once belonged to Alejandro Muñoz Moreno, \
+        a very formidable foe. There is no lucha libre tournament without one of these \
+        classic faces.",
         active: "",
       },
     },
-    luchMask: "/CreativeProject4/images/masks/la_muerte.svg",
   },
   created: function(){
+    this.curIndex = 0;
+    this.images[this.curIndex].active = true;
   },
   watch:{
     curIndex: function(newVal, oldVal) {
@@ -56,7 +72,9 @@ var appOne = new Vue({
         this.curIndex = 0;
       else if (this.curIndex < 0)
         this.curIndex = maxLen - 1;
-      this.luchMask = this.images[this.curIndex].url;
+
+      if (oldVal < 0) oldVal = 0;
+      else if (oldVal >= maxLen) oldVal = maxLen - 1;
 
       this.images[oldVal].active = false;
       this.images[this.curIndex].active = true;
